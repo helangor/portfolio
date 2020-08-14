b2e910a30ddcb519d44ae62f9f846e1c6431fd1d
helangor <henrikki_helander@hotmail.com>
2020-08-14 14:22:53 +0300

    Updates

---
+++ precache-manifest.4e74dfdb2a15455c6bf07aa0786ab511.js
@@ -0,0 +1,122 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aca6a49978875a0406b121aa20eb15f1",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "8e0b17c693bb38939ea7",
+    "url": "/portfolio/static/css/main.b21b9036.chunk.css"
+  },
+  {
+    "revision": "a7df038f0b57c4cb4966",
+    "url": "/portfolio/static/js/2.edca357e.chunk.js"
+  },
+  {
+    "revision": "8e0b17c693bb38939ea7",
+    "url": "/portfolio/static/js/main.29594a9d.chunk.js"
+  },
+  {
+    "revision": "76aa8cbd67b5c937ec2c",
+    "url": "/portfolio/static/js/runtime-main.e92c632a.js"
+  },
+  {
+    "revision": "21f233e19402cc4a66866a7f31191f0d",
+    "url": "/portfolio/static/media/Agustina.21f233e1.woff"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "/portfolio/static/media/Montserrat-Regular.ee653992.ttf"
+  },
+  {
+    "revision": "d296e1388b8f01eabf9038652e83deaa",
+    "url": "/portfolio/static/media/PhoneStartCar.d296e138.png"
+  },
+  {
+    "revision": "11a8091b788dddc01af94c8b65d8520d",
+    "url": "/portfolio/static/media/businessGuy.11a8091b.svg"
+  },
+  {
+    "revision": "59a68c263314d7f380c4cabfb938d646",
+    "url": "/portfolio/static/media/businessmangreets.59a68c26.svg"
+  },
+  {
+    "revision": "ad85d35bde1f94ce914b2d742b576e90",
+    "url": "/portfolio/static/media/businessmanspeaks.ad85d35b.svg"
+  },
+  {
+    "revision": "06147b6cd88c7346cecd1edd060cd5de",
+    "url": "/portfolio/static/media/fa-brands-400.06147b6c.ttf"
+  },
+  {
+    "revision": "5063b105c7646c8043d58c5289f02cca",
+    "url": "/portfolio/static/media/fa-brands-400.5063b105.eot"
+  },
+  {
+    "revision": "a9c4bb7348f42626454c988dbde1d0a0",
+    "url": "/portfolio/static/media/fa-brands-400.a9c4bb73.svg"
+  },
+  {
+    "revision": "c5e0f14f88a828261ba01558ce2bf26f",
+    "url": "/portfolio/static/media/fa-brands-400.c5e0f14f.woff"
+  },
+  {
+    "revision": "cccc9d29470e879e40eb70249d9a2705",
+    "url": "/portfolio/static/media/fa-brands-400.cccc9d29.woff2"
+  },
+  {
+    "revision": "65b286af947c0d982ca01b40e1fcab38",
+    "url": "/portfolio/static/media/fa-regular-400.65b286af.ttf"
+  },
+  {
+    "revision": "7b9568e6389b1f8ae0902cd39665fc1e",
+    "url": "/portfolio/static/media/fa-regular-400.7b9568e6.svg"
+  },
+  {
+    "revision": "c1a866ec0e04a5e1915b41fcf261457c",
+    "url": "/portfolio/static/media/fa-regular-400.c1a866ec.eot"
+  },
+  {
+    "revision": "c4f508e7c4f01a9eeba7f08155cde04e",
+    "url": "/portfolio/static/media/fa-regular-400.c4f508e7.woff"
+  },
+  {
+    "revision": "f5f2566b93e89391da4db79462b8078b",
+    "url": "/portfolio/static/media/fa-regular-400.f5f2566b.woff2"
+  },
+  {
+    "revision": "0bff33a5fd7ec390235476b4859747a0",
+    "url": "/portfolio/static/media/fa-solid-900.0bff33a5.ttf"
+  },
+  {
+    "revision": "333bae208dc363746961b234ff6c2500",
+    "url": "/portfolio/static/media/fa-solid-900.333bae20.woff"
+  },
+  {
+    "revision": "44d537ab79f921fde5a28b2c1636f397",
+    "url": "/portfolio/static/media/fa-solid-900.44d537ab.woff2"
+  },
+  {
+    "revision": "8e4a6dcc692b3887f9f542cd6894d6d4",
+    "url": "/portfolio/static/media/fa-solid-900.8e4a6dcc.eot"
+  },
+  {
+    "revision": "c2801fb415f03c7b170934769d7b5397",
+    "url": "/portfolio/static/media/fa-solid-900.c2801fb4.svg"
+  },
+  {
+    "revision": "a95a654d7e4572dca1c4d3f5c8822055",
+    "url": "/portfolio/static/media/introscreenPriceAnalyser.a95a654d.png"
+  },
+  {
+    "revision": "0a4347797e7dc6e82ed3495a0db18b8f",
+    "url": "/portfolio/static/media/laptopGuy.0a434779.svg"
+  },
+  {
+    "revision": "d414fb7c38cd7a27827520fc50f22e4e",
+    "url": "/portfolio/static/media/sauna.d414fb7c.png"
+  },
+  {
+    "revision": "659d6d5e7e829cdcfe049a15bd6a8063",
+    "url": "/portfolio/static/media/wrenchGuy.659d6d5e.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.f2c873f85cbc9b4822424549be56ec05.js"
+  "/portfolio/precache-manifest.4e74dfdb2a15455c6bf07aa0786ab511.js"
 );
 
 self.addEventListener('message', (event) => {
